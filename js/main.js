24a46b669b20cf0846fd8994d84f7a0e2cd258b5
James Thomas <jamthomas@protonmail.com>
2021-08-17 10:11:32 +0900

    Add data array to js file

--- js/main.js
+++ js/main.js
@@ -1,46 +1,110 @@
+let dataArray = [
+  {
+    title: "The only sin is ignorance",
+    body: "Christopher Marlowe",
+    image_url: "https://picsum.photos/300/300",
+    image_left: true,
+  },
+  {
+    title:
+      "A man is his own easiest dupe, for what he wishes to be true he generally believes to be true",
+    body: "Demosthenes",
+    image_url: "https://picsum.photos/300/300",
+    image_left: true,
+  },
+  {
+    title:
+      "A lie can travel halfway around the world while the truth is putting on its shoes",
+    body: "Mark Twain",
+    image_url: "https://picsum.photos/300/300",
+    image_left: true,
+  },
+  {
+    title:
+      "Great minds discuss ideas; average minds discuss events; small minds discuss people",
+    body: "Eleanor Roosevelt",
+    image_url: "https://picsum.photos/300/300",
+    image_left: true,
+  },
+  {
+    title: "If you have a garden and a library, you have everything you need",
+    body: "Marcus Tullius Cicero",
+    image_url: "https://picsum.photos/300/300",
+    image_left: true,
+  },
+  {
+    title: "Truth comes out in wine",
+    body: "Pliny the Elder",
+    image_url: "https://picsum.photos/300/300",
+    image_left: true,
+  },
+  {
+    title: "Everything in the universe is within you. Ask all from yourself",
+    body: "Rumi",
+    image_url: "https://picsum.photos/300/300",
+    image_left: true,
+  },
+  {
+    title:
+      "When I get a little money I buy books; and if any is left I buy food and clothes",
+    body: "Desiderius Erasmus",
+    image_url: "https://picsum.photos/300/300",
+    image_left: true,
+  },
+]
+
+let staticArray = [
+  {
+    button_copy: "Back to ZenMarket.jp",
+    redirect_copy:
+      'You will be redirected to the homepage in <span id="countdown">10</span> secs',
+  },
+]
+
 $(document).ready(function () {
-    $.getJSON("js/data.json", function (data) { // get JSON file
-        
-        // logs
-        
-        //console.log(data);
-        //console.log(Object.values(data));
-        //console.log(Math.floor(Math.random() * Object.keys(data).length))
+  // logs
+  //console.log(dataArray);
+  //console.log(Object.values(dataArray));
+  //console.log(Math.floor(Math.random() * Object.keys(dataArray).length))
 
-        function random404() {
-            let random = data[Math.floor(Math.random() * Object.keys(data).length)]; // Cycle through data randomly
-            let static = data[0]; // I want this data to be seperate from the randomize and it stays same each time
-            
-            // Change the HTML
-            title.innerText = random.title;
-            body.innerText = random.body;
-            button_copy.innerText = static.button_copy;
-            redirect_copy.innerHTML = static.redirect_copy;
-            heroimage.src = random.image_url;
-        }
-        // Show random quote
-        random404();
+  let timeleft = 11 //set timer
 
-        // Randomize on click
-        document.querySelector("button").addEventListener('click', random404)
+  function countdown() {
+    timeleft-- // take away 1 per run through function
+    document.getElementById("countdown").textContent = " " + timeleft + " " // change text based on timeleft value
+    if (timeleft <= 0) setTimeout(redirect)
+    // if timeleft is less than or equal to run redirect
+    else setTimeout(countdown, 1000) // else keep counting down in values of 1 second
+  }
 
-    }).fail(function () {
-        console.log("An error has occurred.");
-    });
-});
+  function redirect() {
+    window.location = "https://zenmarket.jp/" //redirect to homepage
+  }
 
-var timeleft = 11; //set timer
-function countdown() {
-    timeleft--; // take away 1 per run through function
-    document
-        .getElementById("countdown")
-        .textContent = " " + timeleft + " "; // change text based on timeleft value
-    if (timeleft <= 0)
-        setTimeout(redirect); // if timeleft is less than or equal to run redirect
-    else
-        setTimeout(countdown, 1000); // else keep counting down in values of 1 second
-}
-function redirect() {
-    window.location = "https://zenmarket.jp/"; //redirect to homepage
-}
-countdown(); // run through countdown function again
+  function random404() {
+    let random =
+      dataArray[Math.floor(Math.random() * Object.keys(dataArray).length)] // Cycle through data randomly
+    let static = staticArray[0] // I want this data to be seperate from the randomize and it stays same each time
+
+    // Change the HTML
+    title.innerText = random.title
+    body.innerText = random.body
+    button_copy.innerText = static.button_copy
+    redirect_copy.innerHTML = static.redirect_copy
+    heroimage.src = random.image_url
+  }
+
+  // Show random quote
+  random404()
+
+  //start countdown
+  countdown()
+
+  // Randomize on click
+  document.querySelector("button").addEventListener("click", function () {
+    // show a new random quote
+    random404()
+    //reset timer to 10
+    timeleft = 11
+  })
+})
